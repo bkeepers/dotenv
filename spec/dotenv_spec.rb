6cf29f7c1be6ab1ac2fe70149885036b1a866044
Brandon Keepers <brandon@opensoul.org>
2012-08-08 10:39:54 -0400

    reorganize spec

--- spec/dotenv_spec.rb
+++ spec/dotenv_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Dotenv::Environment do
-  let(:env_path) { fixture_path('plain.env') }
   let(:dotenv)   { Dotenv::Environment.new(env_path) }
 
   before do
@@ -12,20 +11,38 @@ describe Dotenv::Environment do
     ENV.delete_if { |k,v| !@env_keys.include?(k) }
   end
 
-  describe 'initialize' do
-    it 'reads environment config' do
-      expect(dotenv['OPTION_A']).to eq('1')
-      expect(dotenv['OPTION_B']).to eq('2')
+  context 'with a plain env file' do
+    let(:env_path) { fixture_path('plain.env') }
+
+    describe 'initialize' do
+      it 'reads environment config' do
+        expect(dotenv['OPTION_A']).to eq('1')
+        expect(dotenv['OPTION_B']).to eq('2')
+      end
     end
-    it 'fails silently if there is no file' do
-      lambda { Dotenv::Environment.new('.env_does_not_exist') }.should_not raise_error
+
+    describe 'apply' do
+      it 'sets variables in ENV' do
+        dotenv.apply
+        expect(ENV['OPTION_A']).to eq('1')
+      end
     end
   end
 
-  describe 'apply' do
-    it 'sets variables in ENV' do
-      dotenv.apply
-      expect(ENV['OPTION_A']).to eq('1')
+  context 'when the file does not exist' do
+    let(:env_path) { fixture_path('.env_does_not_exist') }
+
+    describe 'initialize' do
+      it 'fails silently' do
+        lambda { Dotenv::Environment.new('.env_does_not_exist') }.should_not raise_error
+      end
+    end
+
+    describe 'apply' do
+      it 'does not effect env' do
+        dotenv.apply
+        expect(ENV.keys).to eq(@env_keys)
+      end
     end
   end
 
