23053800eac71976e681a1df9598b03233388616
Brandon Keepers <brandon@opensoul.org>
2012-09-06 11:16:13 -0400

    Add support for loading multiple files
      Dotenv.load '.env', '.env.test'

--- spec/dotenv_spec.rb
+++ spec/dotenv_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe Dotenv::Environment do
-  let(:dotenv)   { Dotenv::Environment.new(env_path) }
+describe Dotenv do
+  let(:env_path) { fixture_path('plain.env') }
 
   before do
     @env_keys = ENV.keys
@@ -11,39 +11,74 @@ describe Dotenv::Environment do
     ENV.delete_if { |k,v| !@env_keys.include?(k) }
   end
 
-  context 'with a plain env file' do
-    let(:env_path) { fixture_path('plain.env') }
-
-    describe 'initialize' do
-      it 'reads environment config' do
-        expect(dotenv['OPTION_A']).to eq('1')
-        expect(dotenv['OPTION_B']).to eq('2')
+  describe 'load' do
+    context 'with no args' do
+      it 'defaults to .env' do
+        Dotenv::Environment.should_receive(:new).with('.env').
+          and_return(mock(:apply => {}))
+        Dotenv.load
       end
     end
 
-    describe 'apply' do
-      it 'sets variables in ENV' do
-        dotenv.apply
-        expect(ENV['OPTION_A']).to eq('1')
+    context 'with multiple files' do
+      let(:expected) do
+        {'OPTION_A' => '1', 'OPTION_B' => '2', 'DOTENV' => 'true'}
+      end
+
+      subject do
+        Dotenv.load('.env', env_path)
+      end
+
+      it 'loads all files' do
+        subject
+        expected.each do |key, value|
+          expect(ENV[key]).to eq(value)
+        end
+      end
+
+      it 'returns hash of loaded environments' do
+        expect(subject).to eq(expected)
       end
     end
   end
 
-  context 'when the file does not exist' do
-    let(:env_path) { fixture_path('.env_does_not_exist') }
+  describe Dotenv::Environment do
+    subject { Dotenv::Environment.new(env_path) }
 
-    describe 'initialize' do
-      it 'fails silently' do
-        expect { Dotenv::Environment.new('.env_does_not_exist') }.not_to raise_error
+    context 'with a plain env file' do
+
+      describe 'initialize' do
+        it 'reads environment config' do
+          expect(subject['OPTION_A']).to eq('1')
+          expect(subject['OPTION_B']).to eq('2')
+        end
+      end
+
+      describe 'apply' do
+        it 'sets variables in ENV' do
+          subject.apply
+          expect(ENV['OPTION_A']).to eq('1')
+        end
       end
     end
 
-    describe 'apply' do
-      it 'does not effect env' do
-        dotenv.apply
-        expect(ENV.keys).to eq(@env_keys)
+    context 'when the file does not exist' do
+      let(:env_path) { fixture_path('.env_does_not_exist') }
+
+      describe 'initialize' do
+        it 'fails silently' do
+          expect { Dotenv::Environment.new('.env_does_not_exist') }.not_to raise_error
+        end
+      end
+
+      describe 'apply' do
+        it 'does not effect env' do
+          subject.apply
+          expect(ENV.keys).to eq(@env_keys)
+        end
       end
     end
+
   end
 
   def fixture_path(name)
