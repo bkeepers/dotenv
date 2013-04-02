55ace642201c8e4f0b3cab530551453123c1c522
Patrick Ellis <patrick@soundcloud.com>
2013-04-01 22:18:59 -0700

    add Dotenv.load!
    `Dotenv.load!` is exactly the same as `Dotenv.load` except it fails fast and hard by raising
    an instance of `Errno::ENOENT` if any of the filename arguments don't exist.
    
    This changeset also includes some small refactoring to dotenv.rb since there was shared
    functionality between the two methods (DRY and all that).

--- lib/dotenv.rb
+++ lib/dotenv.rb
@@ -2,11 +2,23 @@ require 'dotenv/environment'
 
 module Dotenv
   def self.load(*filenames)
-    filenames << '.env' if filenames.empty?
-    filenames.inject({}) do |hash, filename|
-      hash.update Environment.new(filename).apply if File.exists?(filename)
-      hash
+    default_if_empty(filenames).inject({}) do |hash, filename|
+      hash.merge(File.exists?(filename) ? Environment.new(filename).apply : {})
     end
+  end
+
+  # same as `load`, but raises Errno::ENOENT if any files don't exist
+  def self.load!(*filenames)
+    load(
+      *default_if_empty(filenames).each do |filename|
+        raise(Errno::ENOENT.new(filename)) unless File.exists?(filename)
+      end
+    )
+  end
+
+protected
+  def self.default_if_empty(filenames)
+    filenames.empty? ? (filenames << '.env') : filenames
   end
 end
 
