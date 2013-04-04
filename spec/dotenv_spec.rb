60d049efed548157152f8dbd6dc96c3e89038f69
Brandon Keepers <brandon@opensoul.org>
2013-04-04 08:45:34 -0600

    Shared example group for #load and #load!

--- spec/dotenv_spec.rb
+++ spec/dotenv_spec.rb
@@ -1,24 +1,22 @@
 require 'spec_helper'
 
 describe Dotenv do
-  let(:env_path) { fixture_path('plain.env') }
-
-  describe 'load' do
+  shared_examples 'load' do
     context 'with no args' do
+      let(:env_files) { [] }
+
       it 'defaults to .env' do
         Dotenv::Environment.should_receive(:new).with('.env').
           and_return(mock(:apply => {}))
-        Dotenv.load
+        subject
       end
     end
 
     context 'with multiple files' do
+      let(:env_files) { ['.env', fixture_path('plain.env')] }
+
       let(:expected) do
         {'OPTION_A' => '1', 'OPTION_B' => '2', 'DOTENV' => 'true'}
-      end
-
-      subject do
-        Dotenv.load('.env', env_path)
       end
 
       it 'loads all files' do
@@ -32,30 +30,37 @@ describe Dotenv do
         expect(subject).to eq(expected)
       end
     end
+  end
+
+  describe 'load' do
+    subject { Dotenv.load(*env_files) }
+
+    it_behaves_like 'load'
 
     context 'when the file does not exist' do
-      subject { Dotenv.load('.env_does_not_exist') }
+      let(:env_files) { ['.env_does_not_exist'] }
 
       it 'fails silently' do
         expect { subject }.not_to raise_error
         expect(ENV.keys).to eq(@env_keys)
       end
     end
-
   end
 
   describe 'load!' do
-    context 'with multiple files' do
-      context 'when one file exists and one does not' do
-        subject { Dotenv.load!('.env', '.env_does_not_exist') }
+    subject { Dotenv.load!(*env_files) }
 
-        it 'raises an Errno::ENOENT error and does not load any files' do
+    it_behaves_like 'load'
+
+    context 'when one file exists and one does not' do
+      let(:env_files) { ['.env', '.env_does_not_exist'] }
+
+      it 'raises an Errno::ENOENT error and does not load any files' do
+        expect do
           expect do
-            expect do
-              subject
-            end.to raise_error(Errno::ENOENT)
-          end.to_not change { ENV.keys }
-        end
+            subject
+          end.to raise_error(Errno::ENOENT)
+        end.to_not change { ENV.keys }
       end
     end
   end
