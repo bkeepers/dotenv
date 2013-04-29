5084756968badfc1fd783242db093fb9996d6537
Brandon Keepers <brandon@opensoul.org>
2013-04-29 08:32:08 -0400

    Remove Rake include
    This should not be needed https://github.com/rails/rails/pull/1174

--- lib/dotenv/railtie.rb
+++ lib/dotenv/railtie.rb
@@ -2,8 +2,6 @@ require 'dotenv'
 
 module Dotenv
   class Railtie < Rails::Railtie
-    include Rake::DSL if defined?(Rake)
-
     rake_tasks do
       desc 'Load environment settings from .env'
       task :dotenv do
