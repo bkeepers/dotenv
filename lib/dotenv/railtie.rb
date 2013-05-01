f674faf08fc2d76d8027483599e2dc1910e5b7cf
Brandon Keepers <brandon@opensoul.org>
2013-05-01 17:07:57 -0400

    Expose `#env` in controllers and views

--- lib/dotenv/railtie.rb
+++ lib/dotenv/railtie.rb
@@ -2,12 +2,28 @@ require 'dotenv'
 
 module Dotenv
   class Railtie < Rails::Railtie
+    module Env
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :env
+      end
+
+      def env
+        Dotenv.config
+      end
+    end
 
     # Load .env immediately
     Dotenv.load ".env.#{Rails.env}", '.env'
 
     # Expose all the environment variables in config.env
     config.env = Dotenv.config
+
+    config.to_prepare do
+      ActionController::Base.send :include, Dotenv::Railtie::Env
+    end
+
 
     rake_tasks do
       desc 'Load environment settings from .env'
