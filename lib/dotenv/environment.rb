9e55bc37dcbd03378968db97b20cef78b8174bdd
Pavel Pravosud <pavel@pravosud.com>
2013-05-02 21:13:03 +0700

    Allow parser to strip unquoted values

--- lib/dotenv/environment.rb
+++ lib/dotenv/environment.rb
@@ -11,7 +11,7 @@ module Dotenv
       read.each do |line|
         if match = line.match(LINE)
           key, value = match.to_a.drop(1)
-          value = value.sub(/\A(['"])(.*)\1\z/, '\2')
+          value = value.strip.sub(/\A(['"])(.*)\1\z/, '\2')
           value = value.gsub('\n', "\n").gsub(/\\(.)/, '\1') if $1 == '"'
           self[key] = value
         end
