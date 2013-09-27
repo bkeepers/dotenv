bea383bd504aabc934d0584c51645b4d83a53ee5
Samuel Garneau <sgarneau@mirego.com>
2013-09-27 09:12:25 -0400

    Add dotenv_role config variable

--- lib/dotenv/capistrano/recipes.rb
+++ lib/dotenv/capistrano/recipes.rb
@@ -1,9 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
   _cset(:dotenv_path){ "#{shared_path}/.env" }
+  _cset(:dotenv_role){ [:app] }
 
   namespace :dotenv do
     desc "Symlink shared .env to current release"
-    task :symlink, roles: :app do
+    task :symlink, roles: lambda { dotenv_role } do
       run "ln -nfs #{dotenv_path} #{release_path}/.env"
     end
   end
