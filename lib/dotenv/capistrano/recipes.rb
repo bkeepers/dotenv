be12ccc52ae47c0cd104652c166602096000f922
Samuel Garneau <sgarneau@mirego.com>
2013-10-02 13:31:29 -0400

    Execute dotenv:symlink for all roles

--- lib/dotenv/capistrano/recipes.rb
+++ lib/dotenv/capistrano/recipes.rb
@@ -1,10 +1,11 @@
 Capistrano::Configuration.instance(:must_exist).load do
   _cset(:dotenv_path){ "#{shared_path}/.env" }
-  _cset(:dotenv_role){ [:app] }
+
+  symlink_args = (role = fetch(:dotenv_role, nil) ? {roles: role} : {})
 
   namespace :dotenv do
     desc "Symlink shared .env to current release"
-    task :symlink, roles: lambda { dotenv_role } do
+    task :symlink, symlink_args do
       run "ln -nfs #{dotenv_path} #{release_path}/.env"
     end
   end
