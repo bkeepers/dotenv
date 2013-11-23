0d7f1c3324a24e8569957a125f7a53b81caddebb
Marcos Wright-Kuhns <webmaster@kuhnsfam.com>
2013-11-23 11:07:07 -0800

    Split unsupported Regexp into an optional module
    At this point tests are passing in Ruby 1.8.7!

---
+++ lib/dotenv/environment_modern.rb
@@ -0,0 +1,26 @@
+module Dotenv
+  module EnvironmentModern
+    INTERPOLATED_SHELL_COMMAND = /
+      (?<backslash>\\)?
+      \$
+      (?<cmd>             # collect command content for eval
+        \(                # require opening paren
+        ([^()]|\g<cmd>)+  # allow any number of non-parens, or balanced parens (by nesting the <cmd> expression recursively)
+        \)                # require closing paren
+      )
+    /x
+
+    def process_interpolated_shell_commands(value)
+      # Process interpolated shell commands
+      value.gsub(INTERPOLATED_SHELL_COMMAND) do |*|
+        command = $~[:cmd][1..-2] # Eliminate opening and closing parentheses
+
+        if $~[:backslash]
+          $~[0][1..-1]
+        else
+          `#{command}`.chomp
+        end
+      end
+    end
+  end
+end
