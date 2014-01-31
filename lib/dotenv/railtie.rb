8369c1477dceb5677b498dc26aa571c5335957ad
Mark Rebec <mark@markrebec.com>
2014-01-31 15:24:44 -0800

    adds Dotenv::Generator to generate env files from an example file and adds wrapper rake tasks

--- lib/dotenv/railtie.rb
+++ lib/dotenv/railtie.rb
@@ -7,6 +7,21 @@ module Dotenv
       task :dotenv do
         Dotenv.load ".env.#{Rails.env}", '.env'
       end
+
+      namespace :dotenv do
+        desc 'Prompts for values and generates a .env.RAILS_ENV file from .env.example'
+        task :generate, [:target_env] => :environment do |t, args|
+          require 'dotenv/generator'
+          g = Dotenv::Generator.new(args[:target_env] || Rails.env)
+          g.prompt
+          if g.values.empty?
+            STDOUT.puts "No examples or existing keys found. Please create a .env.example file or #{g.target_file} file before running the generator."
+            exit
+          end
+          STDOUT.puts "Writing to file #{g.target_file}"
+          g.write
+        end
+      end
     end
   end
 end
