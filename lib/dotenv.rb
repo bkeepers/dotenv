7efbf0fc8126371bec47e3f94445a17e589aa719
Brandon Keepers <brandon@opensoul.org>
2014-02-22 10:13:10 -0500

    Extract Parser

--- lib/dotenv.rb
+++ lib/dotenv.rb
@@ -1,3 +1,4 @@
+require 'dotenv/parser'
 require 'dotenv/environment'
 
 module Dotenv
--- lib/dotenv/environment.rb
+++ lib/dotenv/environment.rb
@@ -1,28 +1,5 @@
-require 'dotenv/format_error'
-require 'dotenv/substitutions/variable'
-if RUBY_VERSION > '1.8.7'
-  require 'dotenv/substitutions/command'
-end
-
 module Dotenv
   class Environment < Hash
-    @@substitutions = Substitutions.constants.map { |const| Substitutions.const_get(const) }
-
-    LINE = /
-      \A
-      (?:export\s+)?    # optional export
-      ([\w\.]+)         # key
-      (?:\s*=\s*|:\s+?) # separator
-      (                 # optional value begin
-        '(?:\'|[^'])*'  #   single quoted value
-        |               #   or
-        "(?:\"|[^"])*"  #   double quoted value
-        |               #   or
-        [^#\n]+         #   unquoted value
-      )?                # value end
-      (?:\s*\#.*)?      # optional comment
-      \z
-    /x
 
     def initialize(filename)
       @filename = filename
@@ -30,33 +7,11 @@ module Dotenv
     end
 
     def load
-      read.each do |line|
-        if match = line.match(LINE)
-          key, value = match.captures
-
-          value ||= ''
-          # Remove surrounding quotes
-          value = value.strip.sub(/\A(['"])(.*)\1\z/, '\2')
-
-          if $1 == '"'
-            value = value.gsub('\n', "\n")
-            # Unescape all characters except $ so variables can be escaped properly
-            value = value.gsub(/\\([^$])/, '\1')
-          end
-
-          @@substitutions.each do |proc|
-            value = proc.call(value, self)
-          end
-
-          self[key] = value
-        elsif line !~ /\A\s*(?:#.*)?\z/ # not comment or blank line
-          raise FormatError, "Line #{line.inspect} doesn't match format"
-        end
-      end
+      update Parser.parse(read)
     end
 
     def read
-      File.read(@filename).split("\n")
+      File.read(@filename)
     end
 
     def apply
