74667e30d417af5619498f6708a7f0ef12ed5fb7
Brandon Keepers <brandon@opensoul.org>
2014-04-21 22:17:21 -0400

    Remove deprecations

--- lib/dotenv-rails.rb
+++ lib/dotenv-rails.rb
@@ -1,11 +1,1 @@
-require 'dotenv/railtie'
-
-env_file = ".env.#{Rails.env}"
-if File.exists?(env_file) && !defined?(Dotenv::Deployment)
-  warn "Auto-loading of `#{env_file}` will be removed in 1.0. See " +
-    "https://github.com/bkeepers/dotenv-deployment if you would like to " +
-    "continue using this feature."
-  Dotenv.load ".env.#{Rails.env}"
-end
-
 Dotenv.load '.env'
