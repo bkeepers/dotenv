75410044d014fd10667103ba559d9c37579da7b9
Brandon Keepers <brandon@opensoul.org>
2014-06-17 00:12:16 -0400

    .test.env or .env.test

--- lib/dotenv-rails.rb
+++ lib/dotenv-rails.rb
@@ -1,10 +1,14 @@
 require 'dotenv/railtie'
 
-# Load defaults from config/*.env  or .env
+# Load defaults from config/*.env or .env
 Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env").push('.env')
 
 # Override any existing variables if an environment-specific file exists
-Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{Rails.env}").push(".env.#{Rails.env}")
+envs = *Dir.glob("#{Rails.root}/config/**/*.#{Rails.env}.env").push(
+  ".#{Rails.env}.env",
+  ".env.#{Rails.env}"
+)
+Dotenv.overload *envs
 
 # Allow local overrides in .env.local or config/local.env
 Dotenv.overload Rails.root.join('.env.local'), Rails.root.join("config/local.env")
