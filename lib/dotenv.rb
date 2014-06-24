c8d41c23f31167dd53db047f8dd6fa95fd049c1b
Brandon Keepers <brandon@opensoul.org>
2014-06-24 10:56:03 -0400

    extend self instead of defining class methods

--- lib/dotenv.rb
+++ lib/dotenv.rb
@@ -2,24 +2,26 @@ require 'dotenv/parser'
 require 'dotenv/environment'
 
 module Dotenv
-  def self.load(*filenames)
+  extend self
+
+  def load(*filenames)
     with(*filenames) { |f| Environment.new(f).apply if File.exist?(f) }
   end
 
   # same as `load`, but raises Errno::ENOENT if any files don't exist
-  def self.load!(*filenames)
+  def load!(*filenames)
     with(*filenames) { |f| Environment.new(f).apply }
   end
 
   # same as `load`, but will override existing values in `ENV`
-  def self.overload(*filenames)
+  def overload(*filenames)
     with(*filenames) { |f| Environment.new(f).apply! if File.exist?(f) }
   end
 
   # Internal: Helper to expand list of filenames.
   #
   # Returns a hash of all the loaded environment variables.
-  def self.with(*filenames, &block)
+  def with(*filenames, &block)
     filenames << '.env' if filenames.empty?
 
     {}.tap do |hash|
