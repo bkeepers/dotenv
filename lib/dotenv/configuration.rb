61da7c9276e264df68c9ec8bfc1b6f629e9fb916
Brandon Keepers <brandon@opensoul.org>
2014-08-15 07:34:41 -0400

    Refactor into objects to enable introspection

--- lib/dotenv/configuration.rb
+++ lib/dotenv/configuration.rb
@@ -1,62 +1,99 @@
 module Dotenv
   class Configuration
-    def self.string(name, options = {}, &default_block)
-      define_accessor(name, options, default_block) { |value| value }
+    def self.string(name, options = {}, &default)
+      add Variable.new(name, options, &default)
     end
 
-    def self.integer(name, options = {}, &default_block)
-      define_accessor(name, options, default_block) { |value| Integer(value) if value }
+    def self.integer(name, options = {}, &default)
+      add Integer.new(name, options, &default)
     end
 
-    BOOLEANS = {
-      nil => nil, '' => nil,
-      '0' => false, 'false' => false, false => false,
-      '1' => true, 'true' => true, true => true
-    }
+    def self.boolean(name, options = {}, &default)
+      add Boolean.new(name, options, &default)
+    end
 
-    def self.boolean(name, options = {}, &default_block)
-      options = {:suffix => "?"}.merge(options)
-      define_accessor(name, options, default_block) do |value|
-        BOOLEANS.fetch(value) do
-          raise ArgumentError, "invalid value for boolean: #{value.inspect}"
-        end
+    def self.add(variable)
+      accessors.send :define_method, variable.accessor_name do
+        variable.accessor(self)
       end
-    end
-
-    # Internal: Module that the accessors get defined on
-    def self.accessors
-      @accessors ||= Module.new
-    end
-
-    # Internal: Helper method for defining a new accessor
-    #
-    # name - the name of the config variable.
-    # options[:suffix] - the suffix that gets append to the method name
-    def self.define_accessor(name, options = {}, default_block = nil, &cast_block)
-      default_block ||= Proc.new { options[:default] }
-      accessors.send :define_method, "#{name}#{options[:suffix]}" do
-        cast_block.call(variable(name, &default_block)).tap do |value|
-          raise ArgumentError, "#{name} is required" if options[:required] && value.nil?
-        end
-      end
-    end
-
-    # The source for environment variables. Override in subclass to customize.
-    def env
-      ENV
-    end
-
-    # Fetch the variable from the environment. Override in sublass to customize.
-    #
-    # name - the name of an environment variable.
-    # default - a block that returns a default if the variable is not defined.
-    def variable(name, &default)
-      env.fetch(name.to_s.upcase) { instance_eval(&default) }
     end
 
     # Internal: Include accessors into subclass when inheriting this class.
     def self.inherited(subclass)
       subclass.send :include, subclass.accessors
+    end
+
+    class << self
+      # Internal: Module that the accessors get defined on
+      def accessors
+        @accessors ||= Module.new
+      end
+    end
+
+    def env
+      ENV
+    end
+
+    class Variable
+      attr_reader :name, :options
+
+      def initialize(name, options = {}, &default)
+        @name = name
+        @options = options
+        @default = default
+      end
+
+      def required?
+        !!options[:required]
+      end
+
+      def accessor_name
+        name
+      end
+
+      def accessor(context)
+        cast(value(context)).tap do |result|
+          raise ArgumentError, "#{name} is required" if required? && result.nil?
+        end
+      end
+
+      def value(context)
+        context.env.fetch(name.to_s.upcase) do
+          if @default
+            context.instance_eval(&@default)
+          else
+            options[:default]
+          end
+        end
+      end
+
+      def cast(value)
+        value
+      end
+    end
+
+    class Integer < Variable
+      def cast(value)
+        Integer(value) if value
+      end
+    end
+
+    class Boolean < Variable
+      VALUES = {
+        nil => nil, '' => nil,
+        '0' => false, 'false' => false, false => false,
+        '1' => true, 'true' => true, true => true
+      }
+
+      def accessor_name
+        "#{name}?"
+      end
+
+      def cast(value)
+        VALUES.fetch(value) do
+          raise ArgumentError, "invalid value for boolean: #{value.inspect}"
+        end
+      end
     end
   end
 end
