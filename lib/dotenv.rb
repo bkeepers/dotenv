19331a284df3d624d2aff67075953b77dc555545
Aaron Christy <aaron@phad.com>
2014-09-19 13:22:37 -0400

    Do not allow #overload to blow away previously loaded customized environments

--- lib/dotenv.rb
+++ lib/dotenv.rb
@@ -3,25 +3,28 @@ require 'dotenv/environment'
 
 module Dotenv
   def self.load(*filenames)
-    with(*filenames) { |f| Environment.new(f).apply if File.exist?(f) }
+    with_base_env(*filenames) { |f| Environment.new(f).apply if File.exist?(f) }
   end
 
   # same as `load`, but raises Errno::ENOENT if any files don't exist
   def self.load!(*filenames)
-    with(*filenames) { |f| Environment.new(f).apply }
+    with_base_env(*filenames) { |f| Environment.new(f).apply }
   end
 
   # same as `load`, but will override existing values in `ENV`
   def self.overload(*filenames)
-    with(*filenames) { |f| Environment.new(f).apply! if File.exist?(f) }
+    without_base_env(*filenames) { |f| Environment.new(f).apply! if File.exist?(f) }
   end
 
-  # Internal: Helper to expand list of filenames.
-  #
-  # Returns a hash of all the loaded environment variables.
-  def self.with(*filenames, &block)
+  # Internal: Helpers to expand list of filenames.
+  # Returns a hash of all the loaded environment variables (including global .env)
+  def self.with_base_env(*filenames, &block)
     filenames << '.env' if filenames.empty?
+    without_base_env(*filenames, &block)
+  end
 
+  # Returns a hash of all the loaded environment variables
+  def self.without_base_env(*filenames, &block)
     {}.tap do |hash|
       filenames.each do |filename|
         hash.merge! block.call(File.expand_path(filename)) || {}
