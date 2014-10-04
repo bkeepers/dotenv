415eee207ca83f53c9bcc8dec3fe761be0ac6382
Brandon Keepers <brandon@opensoul.org>
2014-10-04 19:25:49 -0400

    Attempt to load spring/watcher
    Fixes #140

--- lib/dotenv/railtie.rb
+++ lib/dotenv/railtie.rb
@@ -4,7 +4,12 @@ module Dotenv
   class Railtie < Rails::Railtie
     config.before_configuration do
       Dotenv.load Rails.root.join('.env')
-      Spring.watch Rails.root.join('.env') if defined?(Spring)
+
+      begin
+        require "spring/watcher"
+        Spring.watch Rails.root.join('.env')
+      rescue LoadError
+      end
     end
   end
 end
