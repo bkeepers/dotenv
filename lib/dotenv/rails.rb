e1a50914da79c7616fa8362212ae30777d082d7f
Brandon Keepers <brandon@opensoul.org>
2014-10-08 12:50:27 -0400

    Override .load class method to avoid calling Kernel#load

--- lib/dotenv/rails.rb
+++ lib/dotenv/rails.rb
@@ -9,9 +9,19 @@ module Dotenv
   class Railtie < Rails::Railtie
     config.before_configuration { load }
 
+    # Public: Load dotenv
+    #
+    # This will get called during the `before_configuration` callback, but you
+    # can manually call `Dotenv::Railtie.load` if you needed it sooner.
     def load
       Dotenv.load Rails.root.join('.env')
       Spring.watch Rails.root.join('.env') if defined?(Spring)
+    end
+
+    # Rails uses `#method_missing` to delegate all class methods to the
+    # instance, which means `Kernel#load` gets called here. We don't want that.
+    def self.load
+      instance.load
     end
   end
 end
