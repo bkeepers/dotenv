a9fb6ca2bae1bcd7ebcee7cfe54721eccf7344db
Joel Van Horn <joel@joelvanhorn.com>
2018-01-12 23:58:31 -0500

    Parse multi-line values properly (e.g. Heroku config export)

--- lib/dotenv/parser.rb
+++ lib/dotenv/parser.rb
@@ -12,7 +12,6 @@ module Dotenv
       [Dotenv::Substitutions::Variable, Dotenv::Substitutions::Command]
 
     LINE = /
-      \A
       \s*
       (?:export\s+)?    # optional export
       ([\w\.]+)         # key
@@ -22,11 +21,10 @@ module Dotenv
         |               #   or
         "(?:\"|[^"])*"  #   double quoted value
         |               #   or
-        [^#\n]+         #   unquoted value
+        [^#\r\n]+         #   unquoted value
       )?                # value end
       \s*
       (?:\#.*)?         # optional comment
-      \z
     /x
 
     class << self
@@ -43,7 +41,12 @@ module Dotenv
     end
 
     def call
-      @string.split(/[\n\r]+/).each do |line|
+      # Process matches
+      @string.scan(LINE).each do |key, value|
+        @hash[key] = parse_value(value || "")
+      end
+      # Process non-matches
+      @string.gsub(LINE, '').split(/[\n\r]+/).each do |line|
         parse_line(line)
       end
       @hash
@@ -52,10 +55,7 @@ module Dotenv
     private
 
     def parse_line(line)
-      if (match = line.match(LINE))
-        key, value = match.captures
-        @hash[key] = parse_value(value || "")
-      elsif line.split.first == "export"
+      if line.split.first == "export"
         if variable_not_set?(line)
           raise FormatError, "Line #{line.inspect} has an unset variable"
         end
