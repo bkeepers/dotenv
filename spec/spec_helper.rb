75b6605dd4b31bfe7c3b6d60a1a6ec6f4bbdc11d
Oliver Peate <oliverp@gmail.com>
2019-08-01 18:00:18 +0100

    Run specs in random order
    https://relishapp.com/rspec/rspec-core/v/3-8/docs/command-line/order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require "dotenv"
 
 RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
   # Restore the state of ENV after each spec
   config.before { @env_keys = ENV.keys }
   config.after  { ENV.delete_if { |k, _v| !@env_keys.include?(k) } }
