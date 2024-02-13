f55afd89bf7cca411d5edb8ecce56d5dde349738
Brandon Keepers <brandon@opensoul.org>
2024-02-13 07:05:22 -0500

    Manually check optional Rails dependency version

--- lib/dotenv/rails.rb
+++ lib/dotenv/rails.rb
@@ -1,3 +1,9 @@
+# Since rubygems doesn't support optional dependencies, we have to manually check
+unless Gem::Requirement.new(">= 6.1").satisfied_by?(Gem::Version.new(Rails.version))
+  warn "dotenv 3.0 only supports Rails 6.1 or later. Use dotenv ~> 2.0."
+  return
+end
+
 require "dotenv"
 require "dotenv/replay_logger"
 require "dotenv/log_subscriber"
