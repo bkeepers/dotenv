54aa1f65dec1ea45354bb3f4c72c9bce200b7c20
Rune Philosof <57357936+runephilosof-abtion@users.noreply.github.com>
2024-09-17 13:02:25 +0200

    Only use Spring if already loaded
    This makes it possible for the user to selectively use Spring on a command to command basis
    
    Fixes https://github.com/bkeepers/dotenv/issues/510

--- lib/dotenv/rails.rb
+++ lib/dotenv/rails.rb
@@ -10,14 +10,11 @@ require "dotenv/log_subscriber"
 Dotenv.instrumenter = ActiveSupport::Notifications
 
 # Watch all loaded env files with Spring
-begin
-  require "spring/commands"
+if defined?(Spring)
   ActiveSupport::Notifications.subscribe("load.dotenv") do |*args|
     event = ActiveSupport::Notifications::Event.new(*args)
     Spring.watch event.payload[:env].filename if Rails.application
   end
-rescue LoadError, ArgumentError
-  # Spring is not available
 end
 
 module Dotenv
